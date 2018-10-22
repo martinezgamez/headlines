d7e3fc3d65c8128b10f90f23fd5eae8b31878a18
martinezgamez <martinezgamez.antonio@gmail.com>
2018-10-22 16:24:16 +0200

    currency and weather

--- headlines.py
+++ headlines.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import feedparser
+import json
+import urllib
+import urllib2
 
 app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -7,18 +10,72 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication':'bbc',
+            'city':'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'}
+
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=dc391a1c7da6487db53b93f4c98d5d1d"
 
 @app.route("/", methods=['GET'])
-def get_news():
-    query = request.args.get("publication")
+def home():
+    publication = request.args.get('publication')
+    if not publication:
+        publication=DEFAULTS['publication']
+
+    articles = get_news(publication)
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+
+    weather = get_weather(city)
+
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from,currency_to)
+
+
+    return render_template("home.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to,
+                           rate=rate, currencies=sorted(currencies))
+
+def get_news(query):
+
     if not query or query.lower() not in RSS_FEEDS:
-        publication = 'bbc'
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
 
-    return render_template('home.html', articles=feed['entries'])
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=7e49ddc751d5ba770d58822462ca04a4"
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                       parsed["weather"][0]["description"],
+                               "temperature": parsed["main"]["temp"],
+                               "city": parsed["name"],
+                               "country": parsed['sys']['country']
+                   }
+    return weather
+
+
+def get_rate(frm,to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
 
 
 if __name__ == '__main__':
